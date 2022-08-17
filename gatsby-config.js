05dffa7684566d8e8b858c5dbda0fa1804c143cb
David Schaeffer <dbschaeffer29@gmail.com>
2022-08-17 14:23:51 -0500

    swapped google fonts plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,17 +1,32 @@
 module.exports = {
 	siteMetadata: {
-		title: `site`,
-		siteUrl: `https://www.yourdomain.tld`,
+		title: `David Schaeffer`,
+		description: `A personal portfolio.`,
+		siteUrl: `https://www.dschaeffer.com`,
 	},
 	plugins: [
 		`gatsby-plugin-styled-components`,
 		`gatsby-plugin-dark-mode`,
 		`gatsby-plugin-netlify`,
+		// {
+		// 	resolve: `gatsby-plugin-google-fonts`,
+		// 	options: {
+		// 		fonts: [`Russo+One`, `Ubuntu+Mono`],
+		// 		display: 'swap',
+		// 	},
+		// },
 		{
-			resolve: `gatsby-plugin-google-fonts`,
+			resolve: `gatsby-plugin-prefetch-google-fonts`,
 			options: {
-				fonts: [`Russo+One`, `Ubuntu+Mono`],
-				display: 'swap',
+				fonts: [
+					{
+						family: `Russo One`,
+					},
+					{
+						family: `Ubuntu Mono`,
+						variants: [`400`, `700`],
+					},
+				],
 			},
 		},
 	],
