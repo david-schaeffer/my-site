c4f2234b834465343a98bc5d25be87d9bd563f9e
dbschaeffer29 <70391103+dbschaeffer29@users.noreply.github.com>
2022-08-23 12:05:14 -0500

    cursor no longer renders on mobile devices

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,12 +3,17 @@ import Sidebar from './Sidebar';
 import Cursor from './Cursor';
 import '../assets/main.css';
 
+const isMobile = () => {
+	const ua = navigator.userAgent;
+	return /Android|Mobi/i.test(ua);
+};
+
 export default function Layout({ children }) {
 	return (
 		<>
 			<Sidebar />
 			<main>{children}</main>
-			<Cursor />
+			{typeof navigator !== 'undefined' && isMobile() ? '' : <Cursor />}
 		</>
 	);
 }
