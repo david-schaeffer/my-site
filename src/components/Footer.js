407c0e8972e27f3fb92a8ce158922fbbaadb5545
David Schaeffer <git@dschaeffer.com>
2022-08-27 14:33:33 -0500

    added footer (& updated readme)

---
+++ src/components/Footer.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default function Footer() {
+	return (
+		<footer>
+			<div className='footer--wrapper'>
+				<p>
+					Designed and built by David Schaeffer. See the code{' '}
+					<a
+						href='https://github.com/dbschaeffer29/my-site'
+						rel='noopener noreferrer'
+						target='_blank'
+					>
+						here
+					</a>
+					.
+				</p>
+			</div>
+		</footer>
+	);
+}
