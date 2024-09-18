e12b32b7f1e3f29901e822744c30840feb94f5cd
David Schaeffer <code@dschaeffer.com>
2024-09-17 20:13:53 -0400

    chore: upgrade yarn, use pnp

--- src/components/sections/About.js
+++ src/components/sections/About.js
@@ -1,58 +1,46 @@
-import React from 'react';
-import { StaticImage } from 'gatsby-plugin-image';
+import React from "react";
+import { StaticImage } from "gatsby-plugin-image";
 
 export default function About() {
-	return (
-		<section id='about' className='section'>
-			<div className='about--wrapper section-wrapper'>
-				<h3 className='about--title section-title'>about me</h3>
-				<p className='about--info'>
-					<span className='accent-text'>Hello!</span> My name is David
-					Schaeffer, and I'm studying Computer Science and Behavioral
-					Neuroscience in the honors program at Northeastern University.
-					<br />
-					<br />
-					My love for coding began when I made my first program (a
-					randomly-generated math test) in MIT's{' '}
-					<a
-						href='https://scratch.mit.edu/'
-						rel='noopener noreferrer'
-						target='_blank'
-					>
-						Scratch
-					</a>{' '}
-					programming language. Now, as I enter my second year of study, I'm
-					interested particularly in the intersection of artificial and natural
-					intelligence, the development of ethical software, and the use of
-					technology to improve quality of life.
-					<br />
-					<br />
-					I'm never one to shy away from a challenge, and I constantly strive to
-					learn more and improve my skills. Recently, I delved into front end
-					development, using tools such as Gatsby, CSS, and Netlify to create
-					this website.
-					<br />
-					<br />
-					Outside my studies, you can find me singing with the{' '}
-					<a
-						href='https://www.nunoreasters.com/'
-						rel='noopener noreferrer'
-						target='_blank'
-					>
-						Nor'easters
-					</a>
-					, experimenting in the kitchen, or exploring Boston.
-				</p>
-				<div className='about--image'>
-					<StaticImage
-						className='img hover'
-						src='../../assets/images/me-sqr.jpg'
-						alt='Headshot'
-						placeholder='tracedSVG'
-						layout='constrained'
-					/>
-				</div>
-			</div>
-		</section>
-	);
+  return (
+    <section id="about" className="section">
+      <div className="about--wrapper section-wrapper">
+        <h3 className="about--title section-title">about me</h3>
+        <p className="about--info">
+          <span className="accent-text">Hello!</span> My name is David Schaeffer, and I'm studying Computer Science and
+          Behavioral Neuroscience in the honors program at Northeastern University.
+          <br />
+          <br />
+          My love for coding began when I made my first program (a randomly-generated math test) in MIT's{" "}
+          <a href="https://scratch.mit.edu/" rel="noopener noreferrer" target="_blank">
+            Scratch
+          </a>{" "}
+          programming language. Now, as I enter my second year of study, I'm interested particularly in the intersection
+          of artificial and natural intelligence, the development of ethical software, and the use of technology to
+          improve quality of life.
+          <br />
+          <br />
+          I'm never one to shy away from a challenge, and I constantly strive to learn more and improve my skills.
+          Recently, I delved into front end development, using tools such as Gatsby, CSS, and Netlify to create this
+          website.
+          <br />
+          <br />
+          Outside my studies, you can find me singing with the{" "}
+          <a href="https://www.nunoreasters.com/" rel="noopener noreferrer" target="_blank">
+            Nor'easters
+          </a>
+          , experimenting in the kitchen, or exploring Boston.
+        </p>
+        <div className="about--image">
+          <StaticImage
+            className="img hover"
+            src="../../assets/images/me-sqr.jpg"
+            alt="Headshot"
+            placeholder="blurred"
+            layout="constrained"
+          />
+        </div>
+      </div>
+    </section>
+  );
 }
